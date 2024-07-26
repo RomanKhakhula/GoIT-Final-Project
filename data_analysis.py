2f2fd3a026f7159f23f033e6a9d58329b902dc73
RomanKhakhula <rkhakhula@gmail.com>
2024-07-26 21:12:06 +0200

    updates in data_analysis for streamlit

--- data_analysis.py
+++ data_analysis.py
@@ -27,21 +27,27 @@ def describe_features(feature, x_dev, dev, x_prob, prob):
   plt.ylabel('avg prob of churn')
   plt.grid(True)
 
-  plt.show()
+  # plt.show()
+  return fig
+
+features_analysis_dict = {}
 
 def analyse_discrete_features(df: pd.DataFrame, list_of_features: list, target_column: chr):
   for feature in list_of_features:
-    print(f'{feature} (describe)' + ' *'*50)
-    print(pd.DataFrame([list(df[feature].describe().values) + [(df[feature].isnull().sum() / df.shape[0])]], columns = list(df[feature].describe().index) + ['part of NaN'], index = None))
-    print('\n')
+    # print(f'{feature} (describe)' + ' *'*50)
+    # print(pd.DataFrame([list(df[feature].describe().values) + [(df[feature].isnull().sum() / df.shape[0])]], columns = list(df[feature].describe().index) + ['part of NaN'], index = None))
+    describe_df = pd.DataFrame([list(df[feature].describe().values) + [(df[feature].isnull().sum() / df.shape[0])]], columns = list(df[feature].describe().index) + ['part of NaN'], index = None)
+    # print('\n')
 
-    print(f'{feature} (frequency / avg prob of {target_column})' + ' *'*50)
+    # print(f'{feature} (frequency / avg prob of {target_column})' + ' *'*50)
     agg_df = df.groupby([feature]).agg({target_column: ['count', 'mean']})
-    print(agg_df.T)
-    print('\n')
+    # print(agg_df.T)
+    # print('\n')
 
-    describe_features(feature, agg_df.index, agg_df[target_column, 'count'], agg_df.index, agg_df[target_column, 'mean'])
-    print('\n'*2)
+    fig = describe_features(feature, agg_df.index, agg_df[target_column, 'count'], agg_df.index, agg_df[target_column, 'mean'])
+    # print('\n'*2)
+    features_analysis_dict[feature] = [describe_df, agg_df.T, fig]
+
 
 def analyse_continuous_features(df: pd.DataFrame, list_of_features: list, target_column: chr):
   for feature in list_of_features:
@@ -52,18 +58,22 @@ def analyse_continuous_features(df: pd.DataFrame, list_of_features: list, target
     rest_shape = df.loc[df[f'{feature}_gr'] == -2].shape[0]
     df.loc[df[f'{feature}_gr'] == -2, f'{feature}_gr'] = [(i // (rest_shape / 10)) + 1 for i in range(rest_shape)]
 
-    print(f'{feature} (describe)' + ' *'*50)
-    print(pd.DataFrame([list(df[feature].describe().values) + [(df[feature].isnull().sum() / df.shape[0])]], columns = list(df[feature].describe().index) + ['part of NaN'], index = None))
-    print('\n')
+    # print(f'{feature} (describe)' + ' *'*50)
+    # print(pd.DataFrame([list(df[feature].describe().values) + [(df[feature].isnull().sum() / df.shape[0])]], columns = list(df[feature].describe().index) + ['part of NaN'], index = None))
+    describe_df = pd.DataFrame([list(df[feature].describe().values) + [(df[feature].isnull().sum() / df.shape[0])]], columns = list(df[feature].describe().index) + ['part of NaN'], index = None)
+    # print('\n')
 
-    print(f'{feature} (frequency / avg prob of {target_column})' + ' *'*50)
+    # print(f'{feature} (frequency / avg prob of {target_column})' + ' *'*50)
     agg_df1 = df.groupby([f'{feature}_gr']).agg({target_column: ['count', 'mean'], feature: ['min','max','mean']})
     agg_df2 = df.groupby([feature]).agg({target_column: ['count']})
-    print(agg_df1.T)
-    print('\n')
+    # print(agg_df1.T)
+    # print('\n')
 
-    describe_features(feature, agg_df2.index, agg_df2[target_column, 'count'], agg_df1[feature, 'mean'], agg_df1['churn', 'mean'])
-    print('\n'*2)
+    fig = describe_features(feature, agg_df2.index, agg_df2[target_column, 'count'], agg_df1[feature, 'mean'], agg_df1['churn', 'mean'])
+    # print('\n'*2)
+    features_analysis_dict[feature] = [describe_df, agg_df1.T, fig]
+
+analyse_discrete_features(init_data_df, discrete_initial, 'churn')
 
 # is_tv_subscriber, is_movie_package_subscriber - без змін
 # service_failure_count - групуємо 4+, OneHotEncod-имо
@@ -76,6 +86,8 @@ work_df[f'service_failure_count_4'] = [1 if el >= 4  else 0 for el in list(work_
 
 for n in range(8):
   work_df[f'download_over_limit_{n}'] = [1 if n == el  else 0 for el in list(work_df['download_over_limit'])]
+
+analyse_continuous_features(work_df, continuous_initial, 'churn')
 
 # subscription_age - відємні значення в 0, нормалізуємо
 # bill_avg - нормалізуємо
@@ -99,11 +111,25 @@ discrete_additional = ['service_failure_count_0', 'service_failure_count_1', 'se
                        'reamining_contract']
 continuous_additial = ['subscription_age_norm', 'bill_avg_norm', 'download_avg_norm','upload_avg_norm']
 
-# corr = work_df[discrete_initial + ['churn']].corr()
-# sns.heatmap(corr, cmap="Blues", annot=True)
+analyse_discrete_features(work_df, discrete_additional, 'churn')
+analyse_continuous_features(work_df, continuous_additial, 'churn')
 
-# corr = work_df[continuous_additial + ['churn']].corr()
-# sns.heatmap(corr, cmap="Blues", annot=True)
+features_lists_dict = {}
+features_lists_dict['initial discrete'] = discrete_initial
+features_lists_dict['initial continuous'] = continuous_initial
+features_lists_dict['processed discrete'] = discrete_additional
+features_lists_dict['processed continuous'] = continuous_additial
+
+correlations_dict = {}
+corr = work_df[discrete_initial + ['churn']].corr()
+fig, ax = plt.subplots()
+ax = sns.heatmap(corr, cmap="Blues", annot=True)
+correlations_dict['initial discrete features'] = fig
+
+corr = work_df[continuous_additial + ['churn']].corr()
+fig, ax = plt.subplots()
+ax = sns.heatmap(corr, cmap="Blues", annot=True)
+correlations_dict['normalized continuous features'] = fig
 
 # на основі аналізу кореляці з фін набору параметрів викидаємо upload_avg
 
@@ -116,4 +142,4 @@ final_df.to_csv('final_df.csv')
 X_train, X_test, y_train, y_test = train_test_split(final_df.loc[:, 'is_tv_subscriber' : 'download_avg_norm'], final_df['churn'], test_size = 0.3, random_state = 42, shuffle = True)
 
 if __name__ == "__main__":
-    print(X_train)
+    print(list(correlations_dict.keys()))
